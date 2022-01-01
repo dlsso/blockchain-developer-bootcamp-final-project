b7e7889828b75c289d30a5953e2beea33e3868e6
John Hamel <dlsso@users.noreply.github.com>
2021-12-31 21:28:39 -0700

    Update to use constructor keyword

--- contracts/Puzzle.sol
+++ contracts/Puzzle.sol
@@ -7,19 +7,19 @@ contract Puzzle {
   event LogSolveAttempt(bool success);
 	
 	// Follows ConsenSys example. Find out difference between this and constructor.
-  function Puzzle(answer) public payable {
+  constructor(answer) public payable {
     hashedAnswer = keccak256(abi.encodePacked(answer));
   }
 
   function solve(answer) public returns (uint) {
 		if(keccak256(abi.encodePacked(answer)) == hashedAnswer){
 			// Correct! Send contract value to solver
-			msg.sender.transfer(address(this).balance) // Upgrade to call?
-			emit LogSolveAttempt(true)
+			msg.sender.transfer(address(this).balance); // Upgrade to call?
+			emit LogSolveAttempt(true);
 			return true;
 		}
 		// Incorrect, notify failure
-		emit LogSolveAttempt(false)
+		emit LogSolveAttempt(false);
   	return false;
   }
 }
