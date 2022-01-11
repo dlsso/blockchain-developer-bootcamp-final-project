8cedc114fbf9c0b3942a4749ba9c067afc94bb8e
John Hamel <dlsso@users.noreply.github.com>
2022-01-11 16:06:18 -0700

    Add NatSpec comments, some formatting

--- contracts/Puzzle.sol
+++ contracts/Puzzle.sol
@@ -1,17 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.12;
 
+/// @title A puzzle creator
+/// @author dlsso
+/// @notice Returns the value stored in the contract to anyone who sends the correct answer
 contract Puzzle {
-  bytes32 hashedAnswer;
+    bytes32 hashedAnswer;
 
-  event LogSolveAttempt(bool success);
+    /// @notice Logs whether or not the solve attemt was successful
+    /// @param success Boolean: whether or not the guess was successful
+    event LogSolveAttempt(bool success);
 	
-  constructor(bytes32 answerHash) public payable {
-    hashedAnswer = answerHash;
-  }
+    constructor(bytes32 answerHash) public payable {
+        hashedAnswer = answerHash;
+    }
 
-  function solve(string memory guess) public returns (bool) {
-		if(keccak256(abi.encodePacked(guess)) == hashedAnswer){
+    /// @notice Returns the value stored in the contract to anyone who sends the correct answer
+    /// @param guess The user's guess what the answer of the puzzle is
+    /// @return Boolean: whether or not the guess was successful
+    function solve(string memory guess) public returns (bool) {
+		if (keccak256(abi.encodePacked(guess)) == hashedAnswer) {
 			// Correct! Send amount in contract to solver's address
             (bool success, ) = msg.sender.call.value(address(this).balance)("");
             // Refund remaining gas if failed
@@ -21,6 +29,6 @@ contract Puzzle {
 		}
 		// Incorrect, notify failure
 		emit LogSolveAttempt(false);
-  	return false;
-  }
+  	    return false;
+    }
 }
--- contracts/PuzzleFactory.sol
+++ contracts/PuzzleFactory.sol
@@ -2,11 +2,19 @@
 pragma solidity 0.6.12;
 import "./Puzzle.sol";
 
-
+/// @title A puzzle creator
+/// @author dlsso
+/// @notice Creates a puzzles with a reward of the value sent to it
 contract PuzzleFactory {
 
+    /// @notice Logs the address and reward amount of the created puzzle
+    /// @param puzzleAddress The address of the created puzzle
+    /// @param reward The amount of the reward in wei
     event LogCreatedPuzzle(address indexed puzzleAddress, uint reward);
 
+    /// @notice Creates a puzzles with a reward of the value sent to it
+    /// @param answer The answer to the puzzle
+    /// @return The address of the new puzzle
     function createPuzzle(string calldata answer) external payable returns(address) {
         bytes32 hashedAnswer = keccak256(abi.encodePacked(answer));
         // Should deploy Puzzle.sol to a new address and give it whatever value was sent
