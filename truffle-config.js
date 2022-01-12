79cff02a0bc9a9cc27bce5d1ad517798e4639a8b
John Hamel <dlsso@users.noreply.github.com>
2022-01-12 09:04:49 -0700

    Add rinkeby deploy option

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraURL = 'https://rinkeby.infura.io/v3/b7d8ba9d703240a89e91f59d414b258a'
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -12,6 +16,11 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, infuraURL),
+      network_id: 4, // Rinkeby's network id
+      gas: 5500000,
     },
     develop: {
       port: 8545
